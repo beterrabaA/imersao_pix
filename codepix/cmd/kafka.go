e6ab6bf8e3389656edb676a0ff22e3c90a04b003
Lucas Vieira <ls6182315@gmail.com>
2023-10-21 23:44:24 -0300

    Update kafka.go

--- codepix/cmd/kafka.go
+++ codepix/cmd/kafka.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+Copyright © 2023 Lucas Vieira ls6182315@gmail.com
 
 */
 package cmd
@@ -14,12 +14,6 @@ import (
 var kafkaCmd = &cobra.Command{
 	Use:   "kafka",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("kafka called")
 	},
