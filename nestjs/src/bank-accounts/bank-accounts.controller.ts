02db3a544930d19dc53c0a66539bb0a53af39012
Beterraba <ls6182315@gmail.com>
2023-11-01 22:25:27 -0300

    create bank account dto

--- nestjs/src/bank-accounts/bank-accounts.controller.ts
+++ nestjs/src/bank-accounts/bank-accounts.controller.ts
@@ -3,13 +3,10 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
-  Delete,
 } from '@nestjs/common';
 import { BankAccountsService } from './bank-accounts.service';
 import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
 
 @Controller('bank-accounts')
 export class BankAccountsController {
@@ -27,19 +24,8 @@ export class BankAccountsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.bankAccountsService.findOne(+id);
+    return this.bankAccountsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateBankAccountDto: UpdateBankAccountDto,
-  ) {
-    return this.bankAccountsService.update(+id, updateBankAccountDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.bankAccountsService.remove(+id);
-  }
 }
