e3e42fc26d180eff1d4bf06486b34177f538dbff
Beterraba <ls6182315@gmail.com>
2023-11-16 14:34:05 -0300

    bank accounts page

--- nextjs/app/bank-accounts/page.tsx
+++ nextjs/app/bank-accounts/page.tsx
@@ -1,6 +1,51 @@
-const page = () => {
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { Typography } from '@mui/material'
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2'
+
+import CardAction from '@/components/CardAction'
+
+import { BankAccount } from '@/models'
+
+export async function getBankAccounts(): Promise<BankAccount[]> {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_NEST_API_URL}/bank-accounts`,
+    {
+      next: {
+        revalidate: 10,
+      },
+    },
+  )
+  return response.json()
+}
+
+const page = async () => {
+  const bankAccounts = await getBankAccounts()
   return (
-    <div>Pagina do banco</div>
+    <div>
+      <Typography variant="h4">Contas bancárias</Typography>
+      <Grid2 container spacing={2} mt={1}>
+        {bankAccounts.map((bankAccount) => (
+          <Grid2 key={bankAccount.id} xs={12} sm={6} md={4}>
+            <CardAction
+              action={async () => {
+                'use server'
+                cookies().set('bankAccountId', bankAccount.id)
+                redirect(`/bank-accounts/${bankAccount.id}/dashboard`)
+              }}
+            >
+              <Typography variant="h5" component="div">
+                {bankAccount.owner_name}
+              </Typography>
+              <Typography component="span">
+                Conta: {bankAccount.account_number}
+              </Typography>
+            </CardAction>
+          </Grid2>
+        ))}
+      </Grid2>
+    </div>
   )
 }
 
